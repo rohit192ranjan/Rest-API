397bba2bcfb4f834fca3a20f48407069df8082ab
Rohit <rohit192ranjan@gmail.com>
2023-02-17 22:22:52 +0530

    Chained Route Handlers

--- app.js
+++ app.js
@@ -23,16 +23,16 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.get("/articles", function(req, res){
+app.route("/articles")
+.get(function(req, res){
     Article.find(function(err, foundArticles){
         if(!err){
             res.send(foundArticles);
         }
         else res.send(err);
     });
-});
-
-app.post("/articles", function(req, res){
+})
+.post(function(req, res){
     const newArticle = new Article({
         title: req.body.title,
         content: req.body.content
@@ -41,9 +41,8 @@ app.post("/articles", function(req, res){
         if(!err) res.send("Successfully added a new article.")
         else res.send(err);
     });
-});
-
-app.delete("/articles", function(req, res){
+})
+.delete(function(req, res){
     Article.deleteMany(function(err){
         if(!err){
             res.send("Successfully deleted all articles.")
